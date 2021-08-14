69353cf09cd4a1da2b83cfbbb97434882cf2d1d5
Bounar64 <rd.bounar@gmail.com>
2021-08-14 18:45:17 +0200

    . Installation de webpack-encore, sass-loader, postcss, autoprefixer

---
+++ src/Entity/Product.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $shortLabel;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $material;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $service;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $discount;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $stock;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stockAlert;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $actived;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getShortLabel(): ?string
+    {
+        return $this->shortLabel;
+    }
+
+    public function setShortLabel(string $shortLabel): self
+    {
+        $this->shortLabel = $shortLabel;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getMaterial(): ?string
+    {
+        return $this->material;
+    }
+
+    public function setMaterial(string $material): self
+    {
+        $this->material = $material;
+
+        return $this;
+    }
+
+    public function getService(): ?bool
+    {
+        return $this->service;
+    }
+
+    public function setService(bool $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getDiscount(): ?bool
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(bool $discount): self
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?bool
+    {
+        return $this->stock;
+    }
+
+    public function setStock(bool $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getStockAlert(): ?int
+    {
+        return $this->stockAlert;
+    }
+
+    public function setStockAlert(int $stockAlert): self
+    {
+        $this->stockAlert = $stockAlert;
+
+        return $this;
+    }
+
+    public function getActived(): ?bool
+    {
+        return $this->actived;
+    }
+
+    public function setActived(bool $actived): self
+    {
+        $this->actived = $actived;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
