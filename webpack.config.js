1bcc81f8d7295462f7e8b73b5cf42e7841fcb438
Bounar64 <rd.bounar@gmail.com>
2021-08-19 12:19:47 +0200

    Customisation de la page d'acceuil

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -9,11 +10,24 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+            //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        pattern: /\.(png|jpg|jpeg|svg)$/
+})
+
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
-
     /*
      * ENTRY CONFIG
      *
@@ -72,4 +86,13 @@ Encore
     //.autoProvidejQuery()
 ;
 
+module.exports  =  { 
+    // ... 
+    plugins : [ new  DashboardPlugin () ]
+    // ...
+  } ;
+             
 module.exports = Encore.getWebpackConfig();
+
+   
+
