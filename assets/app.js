8e0daf91cbf714a567d845a6fbf109251a51639d
Bounar64 <rd.bounar@gmail.com>
2021-08-21 17:18:17 +0200

    Ajout des popovers pour la navbar des catégories

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,58 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+
+
+// ************************************************************************************************************************************************************************** //
+
+// Activation des popovers des catégories //
+$(document).ready(function(){
+    
+    /* $('.contenu-guitare').hide();
+    $('.cat-guitare').click(function(){
+        $('.contenu-guitare').toggle();
+    });*/
+
+        $(".cat-guitare").click(function(){
+            $(this).toggleClass("active");
+            $(".contenu-guitare").toggleClass("active");
+        })
+        $(".contenu-guitare").focusout(function(){
+            $(".contenu-guitare").hide();
+        })    
+        $(".cat-batterie").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-batterie").toggleClass("active");
+        })     
+        $(".cat-clavier").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-clavier").toggleClass("active");
+        })
+        $(".cat-studio").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-studio").toggleClass("active");
+        })
+        $(".cat-sono").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-sono").toggleClass("active");
+        })
+        $(".cat-eclairage").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-eclairage").toggleClass("active");
+        })
+        $(".cat-dj").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-dj").toggleClass("active");
+        })
+        $(".cat-cases").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-cases").toggleClass("active");
+        })
+        $(".cat-access").hover(function(){
+            $(this).toggleClass("active");
+            $(".contenu-access").toggleClass("active");
+        })
+   });
+
